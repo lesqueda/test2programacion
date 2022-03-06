66ba7693477add006dbb33a3538da7a7b1b7dafb
Leonardo Esqueda <lesqueda@soluprime.com.pa>
2022-03-05 20:26:12 -0500

    Nueva Funcionalidad

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -5,5 +5,12 @@ using namespace std;
 int main(int argc, char){
 
     cout << "Hello_World";
-    
+
+    int i = 5;
+    int j = 5;
+    int n = 0; 
+
+    n = i + j;
+
+    cout << "El resultado es: "<< n;
 }
